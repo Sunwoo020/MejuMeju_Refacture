cfbf106ba6fc030939492a410c340d5da6555354
sunwoo020 <sunwoo020@naver.com>
2024-09-17 21:23:49 +0900

    Style: 스타일 통합

--- client/src/pages/salesItems/styles/Common.ts
+++ client/src/pages/salesItems/styles/Common.ts
@@ -1,2 +1,24 @@
 import styled from "styled-components";
-//TODO: Alcohol Detail이랑 Alcohol이랑 common 만들기
+export const ButtonStyle = styled.div`
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  background-color: ${({ theme }) => theme.colors.buttonBackground};
+  border: none;
+  border-radius: 4px;
+  color: ${({ theme }) => theme.colors.buttonText};
+  font-size: 1rem;
+`;
+
+export const ScrollbarStyle = styled.div`
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.scrollbarThumb};
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.scrollbarTrack};
+  }
+`;
