397d2e987cb543f205a14fa17b2031c794e1d515
sunwoo020 <sunwoo020@naver.com>
2024-09-29 23:05:28 +0900

    Refactor: Header Interface 분리

---
+++ client/src/layout/Header/util/interface.ts
@@ -0,0 +1,11 @@
+export interface IHeaderContainerProps {
+  hovering: string;
+  y: number;
+  pathname?: string;
+  isOpen?: string;
+  style?: string;
+}
+export interface ScrollState {
+  x: number;
+  y: number;
+}
